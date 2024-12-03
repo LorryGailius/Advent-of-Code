8bfe6fde41e20f6f53005a781314262dc3adfd24
Laurynas Gailius <laurynas.gailius@twoday.com>
2024-12-03 14:04:03 +0200

    Day01

--- src/Utils/Input.cs
+++ src/Utils/Input.cs
@@ -13,4 +13,34 @@ public static class Input
         var lines = File.ReadAllLines(path);
         return lines;
     }
+
+    public static List<List<T>> ToLists<T>(string filename) 
+    {
+        var assemblyDirectory = Path.GetDirectoryName(typeof(Input).Assembly.Location)!;
+
+        var projectDirectory = Directory.GetParent(assemblyDirectory)?.Parent?.Parent?.FullName
+                               ?? throw new InvalidOperationException("Could not determine project directory.");
+
+        var path = Path.Combine(projectDirectory, filename);
+
+        using var stream = new StreamReader(path);
+
+        var result = new List<List<T>>();
+
+        var line = stream.ReadLine();
+        var values = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+        var valueCount = values.Length;
+
+        Enumerable.Range(0, valueCount).ToList().ForEach(_ => result.Add([]));
+
+        values.Select((value, index) => (value, index)).ToList().ForEach(x => result[x.index].Add((T)Convert.ChangeType(x.value, typeof(T))));
+
+        while ((line = stream.ReadLine()) != null)
+        {
+            values = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            values.Select((value, index) => (value, index)).ToList().ForEach(x => result[x.index].Add((T)Convert.ChangeType(x.value, typeof(T))));
+        }
+
+        return result;
+    }
 }
--- src/Year2024/Day01/Day01.cs
+++ src/Year2024/Day01/Day01.cs
@@ -2,11 +2,13 @@
 
 namespace Year2024.Day01;
 
+/// https://adventofcode.com/2024/day/1
+
 public class Day01 : BaseDay
 {
     protected override string Day { get; } = "Day01";
-    protected override Answers Part1Answers { get; } = new(0, 0);
-    protected override Answers Part2Answers { get; } = new(0, 0);
+    protected override Answers Part1Answers { get; } = new(11, 1189304);
+    protected override Answers Part2Answers { get; } = new(31, 24349736);
 
     protected override int Solve(string inputFile, int part)
     {
@@ -20,11 +22,34 @@ public class Day01 : BaseDay
 
     private int SolvePart1(string inputFile)
     {
-        return 0;
+        var lists = Input.ToLists<int>(inputFile);
+
+        var list1 = lists[0];
+        var list2 = lists[1];
+
+        list1 = list1.OrderBy(x => x).ToList();
+        list2 = list2.OrderBy(x => x).ToList();
+
+        var diff = list1.Zip(list2).Sum(x => Math.Abs(x.First - x.Second));
+
+        return diff;
     }
 
     private int SolvePart2(string inputFile)
     {
-        return 0;
+        var lists = Input.ToLists<int>(inputFile);
+
+        var list1 = lists[0];
+        var list2 = lists[1];
+
+        list1 = list1.OrderBy(x => x).ToList();
+        list2 = list2.OrderBy(x => x).ToList();
+
+        var similarityScore = list1.Sum(x =>
+        {
+            return list2.Count(y => y == x) * x;
+        });
+
+        return similarityScore;
     }
 }
